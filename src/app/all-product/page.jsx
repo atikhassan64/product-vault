52055dab28db123cc0a5656c3657e379d4ac3d8c
Atik Hassan <atikhassant64@gmail.com>
2025-11-27 08:51:32 +0600

    added some changes

--- src/app/all-product/page.jsx
+++ src/app/all-product/page.jsx
@@ -1,14 +1,34 @@
 
-import AllProducts from '@/component/product/AllProducts'
+// import AllProducts from '@/component/product/AllProducts'
+
+// export default async function Page() {
+//   // Server-side fetch
+//   const res = await fetch("http://localhost:3000/api/event");
+//   const events = await res.json();
+
+//   return (
+//     <div>
+//       <AllProducts events={events} />
+//     </div>
+//   );
+// }
+
+
+import AllProducts from '@/component/product/AllProducts';
+import clientPromise from "@/lib/mongodb";  // <-- তোমার mongo config
 
 export default async function Page() {
-  // Server-side fetch
-  const res = await fetch("http://localhost:3000/api/event");
-  const events = await res.json();
+  const client = await clientPromise;
+  const db = client.db("productVault");
+
+  const products = await db
+    .collection("events")
+    .find({})
+    .toArray();
 
   return (
     <div>
-      <AllProducts events={events} />
+      <AllProducts events={JSON.parse(JSON.stringify(products))} />
     </div>
   );
 }
